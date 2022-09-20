4820c39f8921d4f428f605d1c1c31ab09ff4509e
Ivan Kashin <barbos1701@gmail.com>
2022-09-20 09:20:02 +0300

    moves route config to the app folder (fsd)

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,8 +1,6 @@
 import React, {Suspense} from 'react';
 import {Route, Routes} from "react-router-dom";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
-import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import {routeConfig} from "../lib/routeConfig";
 
 const AppRouter = () => {
   return (
--- src/shared/config/routeConfig/routeConfig.ts
+++ src/shared/config/routeConfig/routeConfig.ts
@@ -11,14 +11,3 @@ export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
 }
-
-export const routeConfig: Record<AppRoutes, RouteProps> = {
-  [AppRoutes.MAIN]: {
-    path: RoutePath.main,
-    element: <MainPage />,
-  },
-  [AppRoutes.ABOUT]: {
-    path: RoutePath.about,
-    element: <AboutPage />,
-  }
-}
