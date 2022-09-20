5d2fe276684128cf86afae045f7a84c9676ac1b5
Ivan Kashin <barbos1701@gmail.com>
2022-09-20 09:15:09 +0300

    2.3) configures app router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,7 @@ import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
@@ -15,12 +14,8 @@ const App = () => {
       <button onClick={toggleTheme} type="button">Переключить тему</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
-        <Suspense fallback={<div>Загрузка...</div>}>
-          <Routes>
-            <Route path={'/about'} element={<AboutPage />}/>
-            <Route path={'/'} element={<MainPage />}/>
-          </Routes>
-        </Suspense>
+
+      <AppRouter />
     </div>
   );
 };
