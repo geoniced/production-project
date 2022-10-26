0152ebda08f43135764faa5d6a76d66f9947bc5b
Ivan Kashin <barbos1701@gmail.com>
2022-10-26 12:25:39 +0600

    15) adds i18n and define plugin

---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,28 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './LangSwitcher.module.scss';
+import {useTranslation} from "react-i18next";
+import React from "react";
+import {Button, ButtonTheme} from "shared/ui/Button/Button";
+
+interface LangSwitcherProps {
+  className?: string;
+}
+
+export const LangSwitcher = ({className}: LangSwitcherProps) => {
+  const {t, i18n} = useTranslation();
+
+  const onToggleClick = () => {
+    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+  }
+
+  return (
+      <Button
+        theme={ButtonTheme.CLEAR}
+        className={classNames(cls.langSwitcher, {}, [className])}
+        type="button"
+        onClick={onToggleClick}
+      >
+        {t('Language')}
+      </Button>
+  )
+};
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Sidebar.module.scss';
 import React, {useState} from "react";
 import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "shared/ui/LangSwitcher/LangSwitcher";
 
 interface SidebarProps {
   className?: string;
@@ -25,7 +26,7 @@ export const Sidebar = ({className}: SidebarProps) => {
 
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* LangSwitcher */}
+        <LangSwitcher className={cls.langSwitcher} />
       </div>
     </div>
   );
