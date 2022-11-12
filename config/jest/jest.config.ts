bd7e36dcd8a86ed2ffaa0d2da5feefaaf0818fe9
Ivan Kashin <barbos1701@gmail.com>
2022-11-12 11:23:17 +0600

    20) adds jest and adds first test suite

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,37 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // All imported modules in your tests should be mocked automatically
+  // automock: false,
+
+  // Stop running tests after `n` failures
+  // bail: 0,
+
+  // The directory where Jest should store its cached dependency information
+  // cacheDirectory: "/private/var/folders/2_/4fy_x4xj5071vqj90vczb2s40000gn/T/jest_dx",
+
+  // Automatically clear mock calls, instances and results before every test
+  clearMocks: true,
+  testEnvironment: 'jsdom',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+  ],
+  moduleDirectories: [
+    'node_modules',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node',
+  ],
+  rootDir: '../../',
+  testMatch: [
+    '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+  ],
+};
