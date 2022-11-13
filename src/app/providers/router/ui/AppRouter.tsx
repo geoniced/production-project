7ca421de8e50839e6d539f43461296020191d587
Ivan Kashin <barbos1701@gmail.com>
2022-11-13 11:40:52 +0600

    21) adds fallback routes, adds preloader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,10 +1,11 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { PageLoader } from 'widgets/PageLoader';
 import { routeConfig } from '../lib/routeConfig';
 
 const AppRouter = () => (
   <div className="page-wrapper">
-    <Suspense fallback={<div>Загрузка...</div>}>
+    <Suspense fallback={<PageLoader />}>
       <Routes>
         {Object.values(routeConfig).map(({ element, path }) => (
           <Route
--- src/shared/config/routeConfig/routeConfig.ts
+++ src/shared/config/routeConfig/routeConfig.ts
@@ -1,13 +1,11 @@
-import { RouteProps } from 'react-router-dom';
-import { MainPage } from 'pages/MainPage';
-import { AboutPage } from 'pages/AboutPage';
-
 export enum AppRoutes {
   MAIN = 'main',
-  ABOUT = 'about'
+  ABOUT = 'about',
+  NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.NOT_FOUND]: '*',
 };
