b70c35187c57f962c08a2a3dae62b84afa4e603a
Ivan Kashin <barbos1701@gmail.com>
2023-04-23 12:04:19 +0600

    gets type from url

--- src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
+++ src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { ThunkConfig } from "app/providers/StoreProvider";
 import { SortOrder } from "shared/types";
-import { ArticleSortField } from "entities/Article";
+import { ArticleSortField, ArticleType } from "entities/Article";
 import { getArticlesPageInited } from "../../selectors/articlesPageSelectors";
 import { articlesPageActions } from "../../slice/articlesPage";
 import { fetchArticlesList } from "../fetchArticlesList/fetchArticlesList";
@@ -16,6 +16,7 @@ export const initArticlesPage = createAsyncThunk<void, URLSearchParams, ThunkCon
       const orderFromUrl = searchParams.get("order") as SortOrder;
       const sortFromUrl = searchParams.get("sort") as ArticleSortField;
       const searchFromUrl = searchParams.get("search");
+      const typeFromUrl = searchParams.get("type") as ArticleType;
 
       if (orderFromUrl) {
         dispatch(articlesPageActions.setOrder(orderFromUrl));
@@ -27,6 +28,10 @@ export const initArticlesPage = createAsyncThunk<void, URLSearchParams, ThunkCon
 
       if (searchFromUrl) {
         dispatch(articlesPageActions.setSearch(searchFromUrl));
+      }
+
+      if (typeFromUrl) {
+        dispatch(articlesPageActions.setType(typeFromUrl));
       }
 
       dispatch(articlesPageActions.initState());
