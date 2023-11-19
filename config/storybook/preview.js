f2c73b1a59fe7fd99505ea468498a4d8d03ece42
Ivan Kashin <barbos1701@gmail.com>
2023-11-19 21:00:57 +0600

    adds theme addon for storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,7 +3,6 @@ import { addDecorator } from "@storybook/react";
 import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
 import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
 import { SuspenseDecorator } from "../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator";
-import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "../../src/shared/const/theme";
 
 export const parameters = {
@@ -15,9 +14,21 @@ export const parameters = {
     },
   },
   layout: "fullscreen",
+  themes: {
+    default: "light",
+    list: [
+      { name: "light", class: ["app", Theme.LIGHT], color: "#ffffff" },
+      { name: "dark", class: ["app", Theme.DARK], color: "#000000" },
+      {
+        name: "typewriter",
+        class: ["app", Theme.TYPEWRITER],
+        color: "#7c7c7c",
+      },
+    ],
+  },
 };
 
 addDecorator(StyleDecorator);
 addDecorator(RouterDecorator);
-addDecorator(ThemeDecorator(Theme.LIGHT));
+// addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(SuspenseDecorator);
