709a190a3238d9ecf6f593e821ce632bc18ce26f
Ivan Kashin <barbos1701@gmail.com>
2023-11-21 10:14:36 +0600

    upgrades webpack config, adds babel cache

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,29 +1,31 @@
-import webpack from 'webpack';
+import webpack from "webpack";
 
-import { buildDevServer } from './buildDevServer';
-import { buildLoaders } from './buildLoaders';
-import { buildPlugins } from './buildPlugins';
-import { buildResolvers } from './buildResolvers';
-import { BuildOptions } from './types/config';
+import { buildDevServer } from "./buildDevServer";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import { BuildOptions } from "./types/config";
 
-export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+export function buildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
   const { paths, mode, isDev } = options;
 
   return {
     mode,
     entry: paths.entry,
     output: {
-      filename: '[name].[contenthash].js',
+      filename: "[name].[contenthash].js",
       path: paths.build,
       clean: true,
-      publicPath: '/',
+      publicPath: "/",
     },
     plugins: buildPlugins(options),
     module: {
       rules: buildLoaders(options),
     },
     resolve: buildResolvers(options),
-    devtool: isDev ? 'inline-source-map' : undefined,
+    devtool: isDev ? "eval-cheap-module-source-map" : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -6,6 +6,8 @@ interface BabelLoaderBuildOptions extends BuildOptions {
 }
 
 export function buildBabelLoader({ isDev, isTsx }: BabelLoaderBuildOptions) {
+  const isProd = !isDev;
+
   const babelRemovePropsPluginConfig = [
     babelRemovePropsPlugin,
     {
@@ -19,18 +21,12 @@ export function buildBabelLoader({ isDev, isTsx }: BabelLoaderBuildOptions) {
     use: {
       loader: "babel-loader",
       options: {
+        cacheDirectory: true,
         presets: ["@babel/preset-env"],
         plugins: [
-          [
-            "i18next-extract",
-            {
-              locales: ["ru", "en"],
-              keyAsDefaultValue: true,
-            },
-          ],
           ["@babel/plugin-transform-typescript", { isTSX: isTsx }],
           "@babel/plugin-transform-runtime",
-          isTsx && !isDev && babelRemovePropsPluginConfig,
+          isTsx && isProd && babelRemovePropsPluginConfig,
           isDev && require.resolve("react-refresh/babel"),
         ].filter(Boolean),
       },
