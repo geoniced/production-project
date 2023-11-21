d4196c020d5ea82f8fa26705c960e4070850dc87
Ivan Kashin <barbos1701@gmail.com>
2023-11-21 21:24:19 +0600

    adds helpers for slices, selectors, actions

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,33 +1,41 @@
 import { memo } from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { Button } from "@/shared/ui/Button";
 
-import { getCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
-import { counterActions } from "../model/slice/counterSlice";
+import { useCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
+import { useCounterActions } from "../model/slice/counterSlice";
 
 export const Counter = memo(function Counter() {
   const dispatch = useDispatch();
-  const counterValue = useSelector(getCounterValue);
+  const counterValue = useCounterValue();
   const { t } = useTranslation();
+  const { increment, decrement, add } = useCounterActions();
 
-  const increment = () => {
-    dispatch(counterActions.increment());
+  const handleIncrement = () => {
+    increment();
   };
 
-  const decrement = () => {
-    dispatch(counterActions.decrement());
+  const handleDecrement = () => {
+    decrement();
+  };
+
+  const handleAddFive = () => {
+    add(5);
   };
 
   return (
     <div>
       <h1 data-testid="value-title">{counterValue}</h1>
-      <Button data-testid="increment-btn" onClick={increment}>
+      <Button data-testid="increment-btn" onClick={handleIncrement}>
         {t("increment")}
       </Button>
-      <Button data-testid="decrement-btn" onClick={decrement}>
+      <Button data-testid="decrement-btn" onClick={handleDecrement}>
         {t("decrement")}
+      </Button>
+      <Button data-testid="add-five-btn" onClick={handleAddFive}>
+        {t("Add five")}
       </Button>
     </div>
   );
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,10 +2,16 @@ import React, { memo } from "react";
 import { useTranslation } from "react-i18next";
 
 import { Page } from "@/widgets/Page";
+import { Counter } from "@/entities/Counter";
 
 const MainPage = memo(function MainPage() {
   const { t } = useTranslation("main");
-  return <Page>{t("Main page")}</Page>;
+  return (
+    <Page>
+      <Counter />
+      {t("Main page")}
+    </Page>
+  );
 });
 
 export default MainPage;
