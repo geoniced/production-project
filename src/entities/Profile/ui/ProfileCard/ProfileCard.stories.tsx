6203325984bc92429f6634c0a9e735ac2290569e
Ivan Kashin <barbos1701@gmail.com>
2023-11-27 10:05:34 +0600

    updates main.yml for github actions

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
-import { Country } from '@/entities/Country';
-import { Currency } from '@/entities/Currency';
+import { Country } from "@/entities/Country";
+import { Currency } from "@/entities/Currency";
+// use static dirs or change webpack config so you can use local paths for storybook builds
+// FYI: lesson #109
 // import avatar from 'shared/assets/tests/storybook.jpeg';
-import { AVATAR } from '@/shared/assets/tests/avatar';
+import { AVATAR } from "@/shared/assets/tests/avatar";
 
-import { ProfileCard } from './ProfileCard';
+import { ProfileCard } from "./ProfileCard";
 
 export default {
-  title: 'entities/ProfileCard',
+  title: "entities/ProfileCard",
   component: ProfileCard,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 } as ComponentMeta<typeof ProfileCard>;
 
-const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />;
+const Template: ComponentStory<typeof ProfileCard> = (args) => (
+  <ProfileCard {...args} />
+);
 
 export const Primary = Template.bind({});
 Primary.args = {
   data: {
-    username: 'admin',
+    username: "admin",
     age: 22,
     country: Country.Kyrgyzstan,
-    lastname: 'Pupkin',
-    firstname: 'Vasya',
-    city: 'Osh',
+    lastname: "Pupkin",
+    firstname: "Vasya",
+    city: "Osh",
     currency: Currency.EUR,
     avatar: AVATAR,
   },
@@ -39,5 +43,5 @@ Loading.args = {
 
 export const WithError = Template.bind({});
 WithError.args = {
-  error: 'error',
+  error: "error",
 };
