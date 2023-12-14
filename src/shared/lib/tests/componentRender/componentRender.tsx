92541a5f442286c613b28e1db3c85abd1e0205c2
Ivan Kashin <barbos1701@gmail.com>
2023-12-14 18:09:01 +0600

    adds tests for isolated components with cypress

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -5,25 +5,50 @@ import { I18nextProvider } from "react-i18next";
 import { MemoryRouter } from "react-router-dom";
 
 import { StateSchema, StoreProvider } from "@/app/providers/StoreProvider";
+// eslint-disable-next-line kashin-fsd-plugin/layer-imports
+import { ThemeProvider } from "@/app/providers/ThemeProvider";
 import i18nForTests from "@/shared/config/i18n/i18nForTests";
+import { Theme } from "@/shared/const/theme";
+// eslint-disable-next-line kashin-fsd-plugin/layer-imports
+import "@/app/styles/index.scss";
 
 export interface ComponentRenderOptions {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+  theme?: Theme;
+}
+
+interface TestProviderProps {
+  children: ReactNode;
+  options?: ComponentRenderOptions;
+}
+
+export function TestProvider(props: TestProviderProps) {
+  const { children, options = {} } = props;
+  const {
+    route = "/",
+    initialState,
+    asyncReducers,
+    theme = Theme.LIGHT,
+  } = options;
+
+  return (
+    <MemoryRouter initialEntries={[route]}>
+      <StoreProvider initialState={initialState} asyncReducers={asyncReducers}>
+        <ThemeProvider initialTheme={theme}>
+          <div className="app">
+            <I18nextProvider i18n={i18nForTests}>{children}</I18nextProvider>
+          </div>
+        </ThemeProvider>
+      </StoreProvider>
+    </MemoryRouter>
+  );
 }
 
 export function componentRender(
   component: ReactNode,
   options: ComponentRenderOptions = {}
 ) {
-  const { route = "/", initialState, asyncReducers } = options;
-
-  return render(
-    <MemoryRouter initialEntries={[route]}>
-      <StoreProvider initialState={initialState} asyncReducers={asyncReducers}>
-        <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
-      </StoreProvider>
-    </MemoryRouter>
-  );
+  return render(<TestProvider options={options}>{component}</TestProvider>);
 }
